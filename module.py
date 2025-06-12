5b1af2580d9676ea1d49a1ca45eff720e3b94d9f
MayaKhurshidAnwar <mayasoftwareengineer783@gmail.com>
2025-06-13 00:16:00 +0500

    initial commit

---
+++ module.py
@@ -0,0 +1,16 @@
+# This is a simple Python module that imports the pyjokes library
+# and prints a random joke to the console.# Make sure to install the pyjokes library before running this code
+# You can install it using pip:
+# pip install pyjokes
+
+import pyjokes
+joke = pyjokes.get_joke()
+print(joke)
+
+
+
+# this is a multiline comment
+"""
+so thanks that
+was my first module
+"""
