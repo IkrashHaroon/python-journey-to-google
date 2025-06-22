d724b8482a524cedd5f38f71bd374fb979133620
MayaKhurshidAnwar <mayasoftwareengineer783@gmail.com>
2025-06-22 17:17:29 +0500

    Added Chapter 4 with practice problems

---
+++ Chapter 4/01_list.py
@@ -0,0 +1,6 @@
+list = ["Apple","Orange",5,75.88,"Ikrash","Zohan"]
+
+print(list[0])
+
+list[0] = "Yubari Melon" # unlike strings list are mutable
+print(list[0])
---
+++ Chapter 4/02_list_methods.py
@@ -0,0 +1,10 @@
+list = ["Apple","Orange",5,75.88,"Ikrash","Zohan"]
+
+list.append("Haroon")
+print(list)
+
+l1 = [84,4,25,2,78,3,54,1]
+# l1.sort()
+# l1.reverse()
+l1.insert(4, 91)
+print(l1)
